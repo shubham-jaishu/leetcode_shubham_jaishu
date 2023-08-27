2214fe8e21fc650504ccfb53b3f3dd8556e39a2f
Shubham Kumar <109282657+shubham-jaishu@users.noreply.github.com>
2023-08-27 15:05:43 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.cpp
+++ Reverse a String - GFG/reverse-a-string.cpp
@@ -12,7 +12,7 @@ using namespace std;
 class Solution
 {
     public:
-  string reverseWord(string str){
+    string reverseWord(string str){
     int i=0,j=str.size()-1;
     while(i<j){
         swap(str[i++],str[j--]);
