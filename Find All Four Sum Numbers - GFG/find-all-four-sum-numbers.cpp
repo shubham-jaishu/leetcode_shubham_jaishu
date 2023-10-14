72efc5917231564b5ebe3f5ace908e3dd75e50f2
Shubham Kumar <109282657+shubham-jaishu@users.noreply.github.com>
2023-10-14 19:39:00 +0530

    Added solution - LeetHub

--- Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
+++ Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
@@ -4,47 +4,41 @@ using namespace std;
 
 
 // } Driver Code Ends
-// User function template for C++
 
-class Solution{
-    public:
-    // arr[] : int input array of integers
-    // k : the quadruple sum required
-    vector<vector<int> > fourSum(vector<int> &arr, int k) {
-        // Your code goes here
+class Solution {
+public:
+    std::vector<std::vector<int>> fourSum(std::vector<int>& arr, int k) {
+        std::vector<std::vector<int>> ans;
         int n = arr.size();
-        sort(arr.begin(), arr.end());
-        vector<vector<int>> ans;
-        set <vector<int>> s;
-        for (int i=0; i<n; i++)
-        {
-            for (int j=i+1; j<n; j++)
-            {
-                int start = j+1;
-                int end = n-1;
-                while (start < end)
-                {
-                    int sum = arr[i]+arr[j]+arr[start]+arr[end];
-                    if (sum == k)
-                    {
-                        s.insert({arr[i],arr[j],arr[start],arr[end]});
-                        start++;
-                        end--;
-                    }
-                    else if (sum > k)
-                    {
-                        end--;
-                    }
-                    else 
-                    {
-                        start++;
+        std::sort(arr.begin(), arr.end());
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue;  // Skip duplicates to avoid duplicate results
+            }
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && arr[j] == arr[j - 1]) {
+                    continue;  // Skip duplicates
+                }
+                int target = k - arr[i] - arr[j];
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    int sum = arr[left] + arr[right];
+                    if (sum == target) {
+                        ans.push_back({arr[i], arr[j], arr[left], arr[right]});
+                        while (left < right && arr[left] == arr[left + 1]) left++;
+                        while (left < right && arr[right] == arr[right - 1]) right--;
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
                     }
                 }
             }
-        }
-        for(auto i:s)
-        {
-            ans.push_back(i);
         }
         return ans;
     }
